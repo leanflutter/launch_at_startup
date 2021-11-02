523d93808125a44eb7374735494edabea627b722
LiJianying <lijy91@foxmail.com>
2021-11-02 21:44:22 +0800

    Add windows support files

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <launch_at_startup/launch_at_startup_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  LaunchAtStartupPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("LaunchAtStartupPlugin"));
+}
