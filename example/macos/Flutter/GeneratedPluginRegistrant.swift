61823ad431759da11dd344121fda77de81068cb3
Emile O <emileoudejans@gmail.com>
2024-05-18 06:40:43 +0200

    Enhancement(macOS): macOS support API change to allow App Sandbox mode (#27)
    * upgraded dependencies, fixed deprecated test methods
    
    * Enhancement(macOS): macOS support API change to allow App Sandbox mode
    
    macOS support API now uses the "LaunchAtLogin" Swift package, thereby allowing App Sandbox mode to be enabled for publishing.
    
    BREAKING CHANGES:
    Additional code needs to be added to the MainFlutterWindow.swift file for platform channel communication and additional configurations are also required to be made via Xcode for the Runner project.
    
    - AppAutoLauncherImplMacOS changed to use new API using Dart Swift platform channels
    - README.md updated with installation instructions for new macOS support API
    
    Example Project:
    - Code added to MainFlutterWindow.swift to import LaunchAtLogin module and handle Dart Swift platform channel communication
    - App Sandbox enabled for Debug, Profile and Release modes in Xcode
    - Configurations added to Xcode Runner project for new API
    - Select packages in pubspec.yaml upgraded by several major versions as old versions were quite outdated and causing unknown issues in the builds
    
    Related to issue #1 > https://github.com/leanflutter/launch_at_startup/issues/1
    
    * Improve README.md formatting, rudimentary Chinese (simplified) translation for README-ZH.md additions

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import package_info_plus_macos
+import package_info_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
 }
